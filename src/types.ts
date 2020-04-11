92a8b72898bb45c658f38df5658751b0764dd691
Victor Winberg <victor.m.winberg@gmail.com>
2020-04-11 08:50:31 +0200

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,8 @@ export interface WeatherCardConfig {
   type: string;
   name?: string;
   title?: string;
-  entity: string;
+  summary: string;
+  temperature: string;
   test_state?: string;
   test_gui?: boolean;
 }
